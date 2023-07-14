2c2b643c7ad2b4d90aa55bad5f4661120b5cb543
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-14 11:20:44 +0800

    Update contents.js

--- src/services/contents.js
+++ src/services/contents.js
@@ -1,1 +1,14 @@
-export const articles = [];
+export const articles = [
+  {
+    title: "Hydrogen vs Electric Cars",
+    body: "Will hydrogen-fueled cars ever to catch up to EVs?",
+  },
+  {
+    title: "The Downside of AI Artistry",
+    body: "What are the possible adverse effects of on-demand AI image generation?",
+  },
+  {
+    title: "Is VC Funding Drying Up?",
+    body: "Private funding by VC firms is down 50% YOY. We take a look at what that means.",
+  },
+];
