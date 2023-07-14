2115df1f55a3bebd92793493d4a66ed54c656edc
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-14 11:36:53 +0800

    Update Article.jsx

--- src/container/Article.jsx
+++ src/container/Article.jsx
@@ -4,7 +4,7 @@ import { articles } from "../services/contents";
 const Article = () => {
   return (
     <div>
-      <h1>New</h1>
+      <h1 className=" text-primary-soft-orange text-4xl font-medium">New</h1>
       <div>
         {articles.map((article, index) => (
           <CardArticle key={index} title={article.title} body={article.body} />
