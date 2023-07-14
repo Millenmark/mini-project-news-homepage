89d6544e1d7380b33c29fa03ada5100e429b1c92
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-14 11:08:36 +0800

    Create Article.jsx

---
+++ src/container/Article.jsx
@@ -0,0 +1,14 @@
+import CardArticle from "../components/CardArticle";
+
+const Article = () => {
+  return (
+    <div>
+      <h1>New</h1>
+      <div>
+        <CardArticle />
+      </div>
+    </div>
+  );
+};
+
+export default Article;
